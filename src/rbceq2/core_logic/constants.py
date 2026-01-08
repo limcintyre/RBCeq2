8b4042b53c645586431cb2e35ef793d67a78e177
Liam <limcintyre@redcrossblood.org.au>
2026-01-08 13:42:24 +1100

    MNS hybrids added to db

--- src/rbceq2/core_logic/constants.py
+++ src/rbceq2/core_logic/constants.py
@@ -1,8 +1,8 @@
 from enum import Enum, auto
 
 # Define version
-VERSION = "2.4.1"
-DB_VERSION = "2.4.1"
+VERSION = "2.4.2"
+DB_VERSION = "2.4.2"
 
 
 COMMON_COLS = ["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT"]
